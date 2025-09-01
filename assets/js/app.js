fc91458da5410d89bda02667222290b413707810
TAKIEDDINE Lombarkia <119461044+TakieddineLom@users.noreply.github.com>
2025-09-01 15:49:25 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,12 +28,12 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="design.html">Design</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="design.html">Gallery</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="event.html">Event</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="sem_temp.html">Sem</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Techstack</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="sem_temp.html">Semester Memories</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Tech Stack</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="travel_temp.html">Travel</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="reference.html">Reference</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="reference.html">References</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
