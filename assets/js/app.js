6cbdc9f0be031e4f3b7494978bd40b09ea3db59c
TAKIEDDINE Lombarkia <119461044+TakieddineLom@users.noreply.github.com>
2025-11-03 10:17:08 +0100

    Reorder navigation items in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,9 +25,9 @@ let header = $(`
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="event.html">Event</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="sem_temp.html">Semester Memories</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Tech Stack</a></li>
